36a8774b883acc91ecfacaf4ab92eed61d9fc769
behrad-tiger <behradhosseini8@gmail.com>
2016-01-27 08:00:20 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {(100777365)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -432,7 +432,7 @@ will return group ban list
 end
 
 function on_our_id (id)
-  our_id = id
+  our_id = 100777365
 end
 
 function on_user_update (user, what)
